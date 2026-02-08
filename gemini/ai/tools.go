4ced522d904a6f5d342e1a5cf5d13dde7e396356
Petr Homola <homola.p@gmail.com>
2026-02-08 10:19:58 +0000

    Gemini copier in tools (#6)
    * using copier for tool input/output
    
    * added omitted error handling

--- gemini/ai/tools.go
+++ gemini/ai/tools.go
@@ -1,15 +1,14 @@
 package ai
 
 import (
-	"encoding/json"
-
+	"github.com/phomola/ai-go/copier"
 	"google.golang.org/genai"
 )
 
 // Tool is an LLM tool with functions.
 type Tool struct {
 	funcDecls []*genai.FunctionDeclaration
-	functions map[string]func([]byte) ([]byte, error)
+	functions map[string]func(map[string]any) (map[string]any, error)
 }
 
 // AddFunction adds a function to a tool.
@@ -29,18 +28,18 @@ func AddFunction[I, O any](tool *Tool, name, description string, f func(*I) (*O,
 		ResponseJsonSchema:   outSchema,
 	})
 	if tool.functions == nil {
-		tool.functions = make(map[string]func([]byte) ([]byte, error))
+		tool.functions = make(map[string]func(map[string]any) (map[string]any, error))
 	}
-	tool.functions[name] = func(inData []byte) ([]byte, error) {
-		var in I
-		if err := json.Unmarshal(inData, &in); err != nil {
-			return nil, err
-		}
-		out, err := f(&in)
+	tool.functions[name] = func(inMap map[string]any) (map[string]any, error) {
+		in, err := copier.FromMap[I](inMap)
 		if err != nil {
 			return nil, err
 		}
-		return json.Marshal(out)
+		out, err := f(in)
+		if err != nil {
+			return nil, err
+		}
+		return copier.ToMap(out)
 	}
 	return nil
 }
