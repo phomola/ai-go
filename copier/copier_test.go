f107d3f322544b4193034d852d3edd2b6c642ff2
Petr Homola <homola.p@gmail.com>
2026-02-08 01:52:57 +0000

    copier from map

--- copier/copier_test.go
+++ copier/copier_test.go
@@ -37,4 +37,41 @@ func TestToMap(t *testing.T) {
 	req.Equal("abcd", m["y"])
 	req.Equal("AB", m["U"].(map[string]interface{})["X"])
 	req.Equal("CD", m["V"].(map[string]interface{})["X"])
+
+	m, err = ToMap(&x)
+	req.Nil(err)
+	req.Equal(4, len(m))
+	req.Equal(1234, m["X"])
+	req.Equal("abcd", m["y"])
+	req.Equal("AB", m["U"].(map[string]interface{})["X"])
+	req.Equal("CD", m["V"].(map[string]interface{})["X"])
+}
+
+func TestFromMap(t *testing.T) {
+	req := require.New(t)
+
+	type (
+		B struct {
+			X string
+		}
+		A struct {
+			X int
+			Y string  `json:"y"`
+			Z float64 `json:"-"`
+			U B
+			V *B
+		}
+	)
+
+	obj, err := FromMap[A](map[string]interface{}{
+		"X": 1234,
+		"y": "abcd",
+		"U": map[string]interface{}{"X": "AB"},
+		"V": map[string]interface{}{"X": "CD"},
+	})
+	req.Nil(err)
+	req.Equal(1234, obj.X)
+	req.Equal("abcd", obj.Y)
+	req.Equal("AB", obj.U.X)
+	req.Equal("CD", obj.V.X)
 }
