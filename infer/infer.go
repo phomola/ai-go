b85eeca75cb197f4eb69e61251511620bebc5e5c
Petr Homola <homola.p@gmail.com>
2026-02-28 00:06:51 +0000

    map conversions

--- infer/infer.go
+++ infer/infer.go
@@ -6,6 +6,7 @@ import (
 	"reflect"
 
 	"github.com/google/jsonschema-go/jsonschema"
+	"github.com/phomola/ai-go/copier"
 )
 
 var (
@@ -19,7 +20,7 @@ type Function struct {
 	Description string
 	InSchema    *jsonschema.Schema
 	OutSchema   *jsonschema.Schema
-	Fn          func(context.Context, any) (any, error)
+	Fn          func(context.Context, map[string]any) (map[string]any, error)
 }
 
 // Functions ...
@@ -35,19 +36,52 @@ func Functions[T any](obj *T) ([]*Function, error) {
 		if m.Type.NumOut() != 2 {
 			return nil, fmt.Errorf("method '%s' must have 2 return values", m.Name)
 		}
-
-		styp := m.Type.In(3)
+		if m.Type.In(1) != contextType {
+			return nil, fmt.Errorf("the first argument of method '%s' must be a context", m.Name)
+		}
+		if m.Type.In(2).Kind() != reflect.Pointer || m.Type.In(2).Elem().Kind() != reflect.Struct {
+			return nil, fmt.Errorf("the second argument of method '%s' must be a pointer to a struct", m.Name)
+		}
+		if m.Type.In(3).Kind() != reflect.Pointer || m.Type.In(3).Elem().Kind() != reflect.Struct {
+			return nil, fmt.Errorf("the third argument of method '%s' must be a pointer to a struct", m.Name)
+		}
+		if m.Type.Out(0).Kind() != reflect.Pointer || m.Type.Out(0).Elem().Kind() != reflect.Struct {
+			return nil, fmt.Errorf("the first return value of method '%s' must be a pointer to a struct", m.Name)
+		}
+		if m.Type.Out(1) != errorType {
+			return nil, fmt.Errorf("the second return value of method '%s' must be an erro", m.Name)
+		}
+		infoType := m.Type.In(3)
 		var methodDesc string
-		f, ok := styp.FieldByName("Info")
+		f, ok := infoType.Elem().FieldByName("Info")
 		if ok {
 			methodDesc = f.Tag.Get("guide")
+		}
+		inType := m.Type.In(2).Elem()
+		outType := m.Type.Out(0).Elem()
+		inSchema, err := jsonschema.ForType(inType, nil)
+		if err != nil {
+			return nil, err
+		}
+		outSchema, err := jsonschema.ForType(outType, nil)
+		if err != nil {
+			return nil, err
 		}
 		funcs = append(funcs, &Function{
 			Name:        m.Name,
 			Description: methodDesc,
-			Fn: func(ctx context.Context, in any) (any, error) {
-				ret := m.Func.Call([]reflect.Value{objPtr, reflect.ValueOf(ctx), reflect.ValueOf(in), reflect.New(styp).Elem()})
-				return ret[0].Interface(), ret[1].Interface().(error)
+			InSchema:    inSchema,
+			OutSchema:   outSchema,
+			Fn: func(ctx context.Context, inMap map[string]any) (map[string]any, error) {
+				in, err := copier.FromMapAny(inMap, inType)
+				if err != nil {
+					return nil, err
+				}
+				ret := m.Func.Call([]reflect.Value{objPtr, reflect.ValueOf(ctx), reflect.ValueOf(in), reflect.Zero(infoType)})
+				if err := ret[1].Interface(); err != nil {
+					return nil, err.(error)
+				}
+				return copier.ToMap(ret[0].Interface())
 			},
 		})
 	}
