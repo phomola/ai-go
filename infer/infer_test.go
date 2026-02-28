246ed79d58414576d18cc546c73d8a6a6f71d5a1
Petr Homola <homola.p@gmail.com>
2026-02-28 00:06:51 +0000

    map conversions

--- infer/infer_test.go
+++ infer/infer_test.go
@@ -2,7 +2,6 @@ package infer
 
 import (
 	"context"
-	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -20,10 +19,13 @@ type output struct {
 
 type tool struct{}
 
-func (t *tool) Func1(_ context.Context, in *input, _ struct {
+func (t *tool) Func1(_ context.Context, in *input, _ *struct {
 	Info any `guide:"GUIDE"`
 }) (*output, error) {
-	return nil, errors.ErrUnsupported
+	return &output{
+		Data: in.Name + in.Name,
+		Num:  in.Age * 2,
+	}, nil
 }
 
 func TestFunctionInference(t *testing.T) {
@@ -34,4 +36,11 @@ func TestFunctionInference(t *testing.T) {
 	req.Equal(1, len(funcs))
 	req.Equal("Func1", funcs[0].Name)
 	req.Equal("GUIDE", funcs[0].Description)
+
+	out, err := funcs[0].Fn(context.Background(), map[string]any{
+		"Name": "John",
+		"Age":  20,
+	})
+	req.Nil(err)
+	req.Equal(map[string]any{"Data": "JohnJohn", "Num": 40}, out)
 }
